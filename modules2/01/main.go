81dcabff96f805591f6c2b6976b3be65697e3f6f
liuxin <liuxin@izaodao.org>
2022-01-06 16:22:53 +0800

    111

---
+++ modules2/01/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func consumer(cname string, ch <-chan int) {
+
+	for i := range ch {
+		fmt.Println("consumer-----", cname, ":", i)
+	}
+
+	fmt.Println("ch closed")
+}
+
+func producer(pname string, ch chan<- int) {
+
+	for i := 0; i < 4; i++ {
+		fmt.Println("prooducer----", pname, ":", i)
+		ch <- i
+	}
+}
+
+func main() {
+	ch := make(chan int)
+	go producer("生产者1", ch)
+	go producer("生产者2", ch)
+	go consumer("消费者1", ch)
+	go consumer("消费者2", ch)
+
+	time.Sleep(10 * time.Second)
+	close(ch)
+	time.Sleep(10 * time.Second)
+}
